a9fc172e5917d0d36ca946011a4e76dc6d65c6e1
elisangela <soareselisangela26@gmail.com>
2022-03-25 11:56:13 -0300

    first commint

---
+++ src/br/com/dio/PrimeiroPragrama.java
@@ -0,0 +1,27 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPragrama {
+
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+	    Livro livro = new Livro();
+	    
+	    System.out.println(gato);
+	    System.out.println(livro);
+		
+		// TODO Auto-generated method stub
+		/*int a = 2;
+		int b = 3;
+        System.out.println("Hello World! " + (a+b));*/
+	}
+
+}
+
+ class Livros {
+	 private  String nome;
+	 private  String npag;
+ }
